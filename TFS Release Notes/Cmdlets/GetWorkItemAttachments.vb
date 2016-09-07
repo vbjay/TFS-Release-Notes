358d123ec8182c751546933ba8a0dfbc0d7d54cb
Jay Asbury <vbjay.net@gmail.com>
2016-09-07 07:46:40 -0400

    Add GetWorkItemAttachments cmdlet

---
+++ TFS Release Notes/Cmdlets/GetWorkItemAttachments.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Management.Automation
+Imports Microsoft.TeamFoundation.WorkItemTracking.Client
+<Cmdlet(VerbsCommon.Get, "WorkItemAttachments")>
+Public Class GetWorkItemAttachments
+    Inherits WorkItemCmdlet
+
+    Protected Overrides Sub ProcessRecord()
+        MyBase.ProcessRecord()
+
+        Dim wa = wi.Select(Function(w) New WorkItemAttachmentsInfo With {.Workitem = w, .Attachments = w.Attachments.Cast(Of Attachment).
+                               OrderBy(Function(a) a.AttachedTime).ToArray}).Where(Function(w) w.Attachments.Any).
+        OrderBy(Function(wc) wc.Attachments.Min(Function(a) a.AttachedTime)).ToArray
+
+        For Each w In wa
+            WriteObject(w)
+        Next
+    End Sub
+End Class
---
+++ TFS Release Notes/Outputs/WorkItemAttachmentsInfo.vb
@@ -0,0 +1,8 @@
+﻿Imports System.IO.Compression
+Imports Microsoft.TeamFoundation.WorkItemTracking.Client
+
+Public Class WorkItemAttachmentsInfo
+    Property Workitem As WorkItem
+    Property Attachments As Attachment()
+
+End Class
