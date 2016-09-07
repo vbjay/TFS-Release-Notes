90774d5014c84b4420b28435b3fc7dc499ca9294
Jay Asbury <vbjay.net@gmail.com>
2016-09-07 07:45:05 -0400

    Rename cmdlet to make clear the functionality

---
+++ TFS Release Notes/Cmdlets/GetWorkItemSQLScripts.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Management.Automation
+Imports System.Text.RegularExpressions
+Imports Microsoft.TeamFoundation.WorkItemTracking.Client
+
+<Cmdlet(VerbsCommon.Get, "WorkItemSQLScripts")>
+Public Class GetWorkItemSQLScripts
+    Inherits WorkItemCmdlet
+
+    Protected Overrides Sub BeginProcessing()
+        MyBase.BeginProcessing()
+
+    End Sub
+    Protected Overrides Sub ProcessRecord()
+        MyBase.ProcessRecord()
+
+        Dim wa = wi.Select(Function(w) New WorkItemAttachmentsInfo With {.Workitem = w, .Attachments = w.Attachments.
+                               Cast(Of Attachment).Where(Function(a) a.Extension.ToLower = ".sql").
+                               OrderBy(Function(a) a.AttachedTime).ToArray}).Where(Function(w) w.Attachments.Any).
+        OrderBy(Function(wc) wc.Attachments.Min(Function(a) a.AttachedTime)).ToArray
+
+        For Each a In wa
+            WriteObject(a)
+        Next
+
+    End Sub
+End Class
