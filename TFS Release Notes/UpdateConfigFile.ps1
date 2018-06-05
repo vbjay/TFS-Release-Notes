9df2a26c62060df2469dc432534a1070e29effaf
Jay Asbury <vbjay.net@gmail.com>
2018-06-05 11:50:30 -0400

    Attempt to add config file to AppDomain

---
+++ TFS Release Notes/UpdateConfigFile.ps1
@@ -0,0 +1,11 @@
+﻿Add-Type -AssemblyName System.Configuration
+
+# Set this to the full path of your App.config
+$configPath = [System.IO.Path]::Combine($PSScriptRoot,'TFS Release Notes.dll.config')
+
+[System.AppDomain]::CurrentDomain.SetData("APP_CONFIG_FILE", $configPath)
+[Configuration.ConfigurationManager].GetField("s_initState", "NonPublic, Static").SetValue($null, 0)
+[Configuration.ConfigurationManager].GetField("s_configSystem", "NonPublic, Static").SetValue($null, $null)
+([Configuration.ConfigurationManager].Assembly.GetTypes() | where {$_.FullName -eq "System.Configuration.ClientConfigPaths"})[0].GetField("s_current", "NonPublic, Static").SetValue($null, $null)
+
+
--- TFS Release Notes/tfs release notes.psd1
+++ TFS Release Notes/tfs release notes.psd1
@@ -57,7 +57,7 @@ Copyright = '(c) 2016 Jay Asbury. All rights reserved.'
 # RequiredAssemblies = @()
 
 # Script files (.ps1) that are run in the caller's environment prior to importing this module.
-# ScriptsToProcess = @()
+ ScriptsToProcess = @('UpdateConfigFile.ps1')
 
 # Type files (.ps1xml) to be loaded when importing this module
 # TypesToProcess = @()
